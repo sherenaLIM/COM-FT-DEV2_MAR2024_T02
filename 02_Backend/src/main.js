f76d4ccb825ffbd1b39a171632b963eebfb15bd9
Joseph Lee <joseph@tradetogether.com>
2024-03-08 00:15:33 +0800

    Edit server
    - Add cors to allow frontend to receive data from backend.
    - Add express.json to handle
    any request body in json format.

--- 02_Backend/src/main.js
+++ 02_Backend/src/main.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const { auth, requiresAuth } = require('express-openid-connect');
 const express = require('express')
+const cors = require('cors');
 
 const app = express()
 const port = process.env.PORT
@@ -13,20 +14,29 @@ const config = {
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
 };
 
+
+//use cors to allow cross origin resource sharing
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
+app.use(express.json());
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
 // req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
-app.get('/profile', requiresAuth(), (req, res) => {
+app.get('/profile', requiresAuth(), async (req, res) => {
   res.send(JSON.stringify(req.oidc.user));
 });
 
-app.post('/users', (req, res) => {
-  console.log(req);
+app.post('/users', async (req, res) => {
+  console.log(req.body);
 })
 
 app.listen(port, () => {
