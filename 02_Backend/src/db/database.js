1c0cd2d37d3eaaf82cd973e2ede7314809269fd2
Joseph Lee <joseph@tradetogether.com>
2024-03-13 09:24:41 +0800

    Setup database driver

---
+++ 02_Backend/src/db/database.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  multipleStatements: true
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error(error);    
+  } else {
+    console.log("MySQL Database connected.");
+  }
+})
+
+module.exports = connection;
